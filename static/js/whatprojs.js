25194fa409b4d0374309a0d01c051f6182b34294
shubham.aggarwal2020@gmail.com <shubham.aggarwal2020@gmail.com>
2017-01-26 11:53:30 +0530

    add comments and fix bugs

--- static/js/whatprojs.js
+++ static/js/whatprojs.js
@@ -14,43 +14,45 @@ var effect = [
 
 $(document).ready(function () {
 
+    // over on top heading
+    // animation effects using animate.cs
     $('#heading').hover(function () {
         $(this).removeClass();
         var index = Math.floor((Math.random() * 10) + 1);
-        $(this).addClass('animated ' + effect[index-1]);
+        $(this).addClass('animated ' + effect[index - 1]);
     }, function () {
         $(this).removeClass();
     });
 
     $("#id_divform").submit(function (event) {
-
+        // prevent for default submitting form
         event.preventDefault();
 
-        $('#extract').prop('disabled', true);
-        $('#reset').prop('disabled', true);
+        var extract_id = $('#extract');
+        var reset_id = $('#reset');
 
+        // disable buttons
+        extract_id.prop('disabled', true);
+        reset_id.prop('disabled', true);
+
+        // get htmlcode from which contacts extracted
         var htmlcode = $('#id_textarea').val();
 
-        if (htmlcode == "") {
-            $('#error-message').val("You Cannot Submit Empty Div Element !");
-            $('.modal').modal();
-            $('#error-modal').modal('open');
-            return;
-        }
-
+        // make a object of data to be send
         var data = {
             "csrfmiddlewaretoken": $('input[name="csrfmiddlewaretoken"]').val(),
             "textarea": htmlcode
         };
 
+        // ajax request to server
         $.ajax({
             type: "POST",
             url: '/',
             data: data,
             dataType: 'json',
             success: function (response) {
-                console.log(response);
-                $('#success-message').val(" Volia ! You have " + response["count"] + " Contacts . Check your downloads");
+                var msg = " Volia ! You have " + response["count"].toString() + " Contacts . Check your downloads";
+                $('#success-message').html(msg);
                 $('.modal').modal();
                 $('#success-modal').modal('open');
                 var file = new File([response["contacts"]], "Contact-List.txt", {type: "text/plain;charset=utf-8"});
@@ -63,10 +65,12 @@ $(document).ready(function () {
             }
         });
 
-        $('#reset').prop('disabled', false);
-        $('#extract').prop('disabled', false);
+        // enable buttons
+        reset_id.prop('disabled', false);
+        extract_id.prop('disabled', false);
     });
 
+    // reset textarea
     $("#reset").on("click", function () {
         $('#div-element').val("");
     });
