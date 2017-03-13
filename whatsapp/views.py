3cb3b6d4b94708ff54cd10777f99d9111af6759a
shubham.aggarwal2020@gmail.com <shubham.aggarwal2020@gmail.com>
2017-03-13 13:08:16 +0530

    add saved and not saved contacts separator

--- whatsapp/views.py
+++ whatsapp/views.py
@@ -7,6 +7,7 @@ from .forms import DivForm
 import requests
 import os
 import sys
+import re
 
 try:
     from whatpro.secret import CAPTCHA_KEY
@@ -15,6 +16,19 @@ except ImportError as im:
     if not SECRET_KEY:
         print(im, " and You don't have CAPTCHA_KEY in environment !")
         sys.exit(0)
+
+
+# Separate saved and not saved contacts
+def separeate(contacts):
+    saved = list()
+    not_saved = list()
+    for contact in contacts:
+        decision = bool(re.search(r'[a-zA-Z]', contact))
+        if decision:
+            saved.append(contact)
+        else:
+            not_saved.append(contact)
+    return saved, not_saved
 
 
 class Index(View):
@@ -41,7 +55,12 @@ class Index(View):
 
             if result.get('success', '') and form.is_valid():
                 contact_list = main(form.cleaned_data["textarea"])
-                return JsonResponse({"contacts": "\r\n".join(contact_list), "count": len(contact_list)})
+                saved, notsaved = separeate(contact_list)
+                contacts = "You Have {} Saved Contacts\n\n".format(len(saved))
+                contacts += "\r\n".join(saved)
+                contacts += "\r\n\r\nYou have {} Contacts which are not saved\n\n".format(len(notsaved))
+                contacts += "\r\n".join(notsaved)
+                return JsonResponse({"contacts": contacts, "count": len(contact_list)})
             else:
                 return JsonResponse({"error": "extraction failed!"})
         else:
