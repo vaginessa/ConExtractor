1af2e63f819a245e0d35f5c4b0ee8dacc2775bac
Shubham Aggarwal <shubham.aggarwal2020@gmail.com>
2017-08-01 00:04:38 +0530

    rm secret.py file

--- whatpro/secret.py
+++
@@ -1,4 +0,0 @@
-SECRET_KEY = '1#5ojx(wz04+^4)3sp36=ln-774gf%@hicf*$tw_*rdlz#tvir'
-CAPTCHA_KEY = '6LfzXhYUAAAAAHUTzQkQDjswu4s__Bq3slBvHSbT'
-MAILCHIMP_API_KEY = 'abf3f0c3eae853fa4418f6b97252501d-us16'
-LIST_ID = '2a0abae869'
